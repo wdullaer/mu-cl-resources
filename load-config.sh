7da6e6c35b24a38530b97813fb547056068fd1b7
Wouter Dullaert <wouter.dullaert@tenfoce.com>
2018-07-03 12:06:42 +0200

    sed the logging configuration based on environment variables

--- load-config.sh
+++ load-config.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# mu-cl-resources does not read env variables for configuration
+# logging is also not handled in mu-cl-resources but the underlying
+# fuseki framework. 
+if [ ${LOG_QUERIES} = 'false' ]; then
+  sed -i 's/:query//' /config/settings.lisp
+  sed -i 's/:ask//' /config/settings.lisp
+fi
+
 cp -R /config/* /app/configuration/
